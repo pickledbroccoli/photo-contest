f3f30b780e7941f15a6da8309de64c803e91df9e
pickledbroccoli <163914034+pickledbroccoli@users.noreply.github.com>
2024-07-16 20:29:30 +0200

    initialiize orm

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+const { Sequelize } = require('sequelize');
+
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(
@@ -14,8 +18,27 @@ app.get('/', (req, res, next) => {
     res.json({info: 'legalabb az express elindult'});
 });
 
+const sequelize = new Sequelize(
+  'photo_contest',
+  process.env.DBUSER,
+  process.env.DBPASSWORD,
+  {
+    host: process.env.DBHOST,
+    dialect: 'postgres'
+  }
+);
 
-
+/*
+const testit = async () => {
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+};
+testit();
+*/
 
 
 
